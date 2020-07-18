c71ae2e28bfab5fd3fc3c6474965e078bf2c24e0
ashishjha0603 <ashishjha0603@gmail.com>
2020-07-18 13:13:38 +0530

    Createed files

---
+++ Sort_without_bubble.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sort_without_bubble {
+public static void main(String args[])
+{
+	Scanner obj = new Scanner(System.in);
+	int i,j,a[],n;
+	n = obj.nextInt();
+	a = new int[n];
+	for(i=0;i<n;i++)
+	{
+		a[i] = obj.nextInt();
+	}
+	Arrays.parallelSort(a);
+	for(i=0;i<n;i++)
+	{
+		System.out.print(a[i]+" ");
+	}
+}
+	
+}
